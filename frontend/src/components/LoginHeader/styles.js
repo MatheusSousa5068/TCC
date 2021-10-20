8c382feff93eb020e248b2f5ed8b6d5906aca69a
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2021-10-20 16:03:21 -0300

    .

---
+++ frontend/src/components/LoginHeader/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    margin-top: 45px;
+    margin-bottom: 80px;
+    align-items: flex-start;
+    flex-direction: row;
+    padding-left: 10px;
+`
+
+export const Text = styled.Text`
+    padding-top: 40px;
+    padding-left: 10px;
+    font-size: 25px;
+`
